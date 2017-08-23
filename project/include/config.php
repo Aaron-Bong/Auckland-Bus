e2794a4e61c9c7231209fbe2976f442c2e5a5381
Ariel Evangelista <aev35@canterbury.ac.nz>
2017-08-23 12:45:24 +1200

    add google map

--- project/include/config.php
+++ project/include/config.php
@@ -3,5 +3,5 @@ $hostname = "csse-info263.canterbury.ac.nz";
 $database = "akl_transport";
 $username = "info263";
 $password = "info263";
-$APIKey = ""; # Your API Key here.
+$APIKey = "8c20ae415fb2473f80e96a47c013b94a"; # Your API Key here. #Ariel Auckland API Key: 8c20ae415fb2473f80e96a47c013b94a
 ?>
--- project/index.php
+++ project/index.php
@@ -3,8 +3,10 @@ $active = "home";
 require_once 'include/header.php';
 ?>
 
+<div id="map"></div>
+
 <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_API_KEY_HERE&callback=initMap">
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWVvlBxK3Hm7BqW97c4cXFKY5wTcpG2vc&callback=initMap">
 </script>
 <script src="scripts/map.js"></script>
 <script>
